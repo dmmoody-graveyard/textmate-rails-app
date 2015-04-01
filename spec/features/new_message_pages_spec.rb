561e931e355e777a6f6385bc11e7fc1321b085dc
Duane M. Moody <dmmoody@gmail.com>
2015-04-01 15:43:24 -0700

    add feature specs for new message

--- spec/features/new_message_pages_spec.rb
+++ spec/features/new_message_pages_spec.rb
@@ -11,4 +11,45 @@ describe 'add new message process' do
     click_on "Send Text"
     expect(page).to have_content "Did it work?"
   end
+
+  it 'shows an error when To is not filled in' do
+    visit messages_path
+    click_on "Send new text"
+    fill_in "To", :with => ""
+    fill_in "From", :with => "5551212121"
+    fill_in "Body", :with => "Did it work?"
+    click_on "Send Text"
+    expect(page).to have_content "errors"
+  end
+
+  it 'shows an error when From is not filled in' do
+    visit messages_path
+    click_on "Send new text"
+    fill_in "To", :with => "5551212121"
+    fill_in "From", :with => ""
+    fill_in "Body", :with => "Did it work?"
+    click_on "Send Text"
+    expect(page).to have_content "errors"
+  end
+
+  it 'shows an error when Body is not filled in' do
+    visit messages_path
+    click_on "Send new text"
+    fill_in "To", :with => "5551212121"
+    fill_in "From", :with => "1112224444"
+    fill_in "Body", :with => ""
+    click_on "Send Text"
+    expect(page).to have_content "errors"
+  end
+
+  it 'shows an error when form is not filled in' do
+    visit messages_path
+    click_on "Send new text"
+    fill_in "To", :with => ""
+    fill_in "From", :with => ""
+    fill_in "Body", :with => ""
+    click_on "Send Text"
+    expect(page).to have_content "errors"
+  end
+
 end
