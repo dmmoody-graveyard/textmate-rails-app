5f226e97b7c2e3bac374e361d2e89600e4a0455a
Duane M. Moody <dmmoody@gmail.com>
2015-04-01 13:45:35 -0700

    Add message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,8 +3,24 @@ class Message < ApplicationController
   def index
     @message = Message.all
   end
-  
 
+  def new
+    @message = Message.new
+  end
 
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      flash[:notice] = "Successfully saved"
+      redirect_to messages_path
+    else
+      render :new
+    end
+  end
+
+private
+  def message_params
+    params.require(:message).permit(:to, :from, :body)
+  end
 
 end
